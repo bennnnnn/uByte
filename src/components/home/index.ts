cd8ae4611d7a8c999a2c4e4d545b781a45190b71
binalfew mecuriaw <binalfewmecuriaw@gmail.com>
2026-05-01 17:38:38 -0700

    remove 10 dead files: Spinner, Testimonials components, ProfileHeader, LangCard, HeroSection, HeroCTAButtons, home barrel, display-list, chat-constants

--- src/components/home/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as LangCard } from "./LangCard";
-export { default as SectionHeading } from "./SectionHeading";
-export { default as HeroSection } from "./HeroSection";
-export { default as HomeHero } from "./HomeHero";
-export { default as TrendingSection } from "./TrendingSection";
-export { default as StepsSection } from "./StepsSection";
-export { default as ValuePropBanner } from "./ValuePropBanner";
--- src/lib/ai/chat-constants.ts
+++
@@ -1,5 +0,0 @@
-/** Maximum characters allowed in a single user chat message. */
-export const MAX_USER_MESSAGE_LENGTH = 300;
-
-/** Maximum number of user turns per chat session. */
-export const MAX_SESSION_TURNS = 10;
